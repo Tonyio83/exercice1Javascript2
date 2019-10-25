dd412f5ce1185ebf50b5ae8f76dbcd93cb638e83
Tonyio83 <anthonyfrancelle@gmail.com>
2019-10-25 14:48:18 +0200

    exercice 1 fini

---
+++ assets/js/index.js
@@ -0,0 +1,3 @@
+var picture = document.getElementById('image1');
+picture.onmouseover = function(){picture.src = 'assets/img/image1_2.jpg'}
+picture.onmouseout = function(){picture.src = 'assets/img/image1.jpg'}
